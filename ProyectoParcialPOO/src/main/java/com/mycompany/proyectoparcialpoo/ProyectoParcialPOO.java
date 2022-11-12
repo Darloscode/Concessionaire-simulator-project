8d03785533415817edd72fa92240eefe8c0edf6f
Carlos Flores <carfgonz@espol.edu.ec>
2022-11-11 23:26:33 -0500

    Primer cambio

--- ProyectoParcialPOO/src/main/java/com/mycompany/proyectoparcialpoo/ProyectoParcialPOO.java
+++ ProyectoParcialPOO/src/main/java/com/mycompany/proyectoparcialpoo/ProyectoParcialPOO.java
@@ -13,5 +13,6 @@ public class ProyectoParcialPOO {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Prueba de commit");
     }
 }

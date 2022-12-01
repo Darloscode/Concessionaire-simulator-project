00516977666ebc91da834f36b3e9769561e78c4c
Carlos Flores <carfgonz@espol.edu.ec>
2022-11-30 23:54:56 -0500

    casi terminado

--- ProyectoParcialPOO/src/main/java/com/mycompany/model/Usuarios/Supervisor.java
+++ ProyectoParcialPOO/src/main/java/com/mycompany/model/Usuarios/Supervisor.java
@@ -45,73 +45,16 @@ public class Supervisor extends Usuario{
         clientes.add(cl);
     }
 
-    /*
-    public void mostrarSolicitudes(ArrayList<Usuario> usuarios){         
-        Scanner sc = new Scanner ( System.in);
-
-        String op= "";
-        String opc= "";
-
-        while(!op.equals("s")){
-            if(solicitudes.size()>0){ 
-                
-                for( int i=0 ; i<solicitudes.size(); i++){
-                    System.out.println((i+1)+ ". El cliente "+clientes.get(i).getNombre()+" ha solicitado comprar un vehiculo de la marca "+ solicitudes.get(i).getMarca()+" y modelo "+solicitudes.get(i).getModelo());
-                    System.out.println(clientes.get(i).mostrarDatos());
-                }   
-
-                System.out.print("\nElija una solicitud para responder o escriba (s) para salir: ");
-                op = sc.nextLine();
-
-                if(isNumeric(op)){
-                    int indice = Integer.parseInt(op);            
-                    if((indice>0)&(indice<=solicitudes.size())){
-                        opc = "";
-                        while(!(opc.equals("3"))){
-                            System.out.println("\n¿Aprobar o Rechazar la solicitud?");
-                            System.out.print("\n1. Aprobar\n2. Rechazar\n3. Salir\nElija una opcion: ");
-                            opc = sc.nextLine();
-                            if(opc.equals("1")){
-                                clientes.get(indice-1).agregarCompra(solicitudes.get(indice-1));
-                                clientes.get(indice-1).agregarMensaje("Ha comprado el vehiculo: "+"\n"+solicitudes.get(indice-1).toString()+"\nPor favor, acerquese al taller para retirarlo");
-                                for(Usuario us : usuarios){
-                                    if(us instanceof JefedeTaller){
-                                        JefedeTaller jdt = (JefedeTaller) us;
-                                        jdt.agregarEntregas(solicitudes.get(indice-1), clientes.get(indice-1));
-                                    }
-                                }
-                                solicitudes.remove(indice-1);
-                                clientes.remove(indice-1);
-                            }else if(opc.equals("2")){
-                                System.out.println("\nEstimado " + getNombre() +", escriba los motivos del rechazo de solicitud de la compra");
-                                System.out.print("\nRespuesta: ");
-                                String respuesta = sc.nextLine();
-                                String mensaje = "Estimado "+clientes.get(indice-1).getNombre()+" la solicitud de compra del vehiculo de la marca "+solicitudes.get(indice-1).getMarca()+" y modelo "+solicitudes.get(indice-1).getModelo()+" ha sido rechazada debido a que: ";
-                                clientes.get(indice-1).agregarMensaje(mensaje+respuesta);
-                                solicitudes.remove(indice-1);
-                                clientes.remove(indice-1);                                
-                            }else if(op.equals("3")){
-
-                            }else{
-                                System.out.println("\nElija una opcion correcta\n");
-                            }
-                        }    
-                    }else{
-                    System.out.println("\nElija una opción de la lista de solicitudes\n");
-                    }                
-                }else if(op.equals("s")){
-                    break;
-                }else{
-                    System.out.println("Elija una opcion correcta");
-                }
-            }else{            
-                break;
-            }
-        }  
-    }  
-    */
-
-    public void mostrarSolicitudes(ArrayList<Usuario> usuarios, ArrayList<Vehiculo> vehiculos){
+    public void consultarStock(ArrayList<Vehiculo> vehiculos){    
+        System.out.println("\n*************************************");            
+        for(Vehiculo vh : vehiculos){            
+            System.out.println(vh.mostrarDatos());
+            System.out.println();
+        }
+        System.out.println("*************************************");
+    }
+ 
+    public void mostrarSolicitudes(ArrayList<Usuario> usuarios, ArrayList<Vehiculo> vehiculos){       
         Scanner sc = new Scanner ( System.in);
 
         String op= "";
@@ -132,17 +75,18 @@ public class Supervisor extends Usuario{
                 if(isNumeric(op)){
                     int indice = Integer.parseInt(op);            
                     if((indice>0)&(indice<=solicitudes.size())){
-                        opc = "";
-                        while(!(opc.equals("3"))){
+                        exit = false;
+                        while(!exit){
                             System.out.println("\n¿Aprobar o Rechazar la solicitud?");
                             System.out.print("\n1. Aprobar\n2. Rechazar\n3. Salir\nElija una opcion: ");
                             opc = sc.nextLine();
                             if(opc.equals("1")){
                                 clientes.get(indice-1).agregarMensaje("Ha comprado el vehiculo: "+"\n"+solicitudes.get(indice-1).toString()+"\nPor favor, acerquese al taller para retirarlo");
-                                if(vehiculos.contains(solicitudes.get(indice-1))){
-                                    int i = vehiculos.indexOf(solicitudes.get(indice-1));
-                                    vehiculos.get(i).setDisponibilidad(Estado.Solicitado);
-                                }
+                                System.out.println("\nEl mensaje ha sido enviado al comprador");
+                                System.out.println(vehiculos.contains(solicitudes.get(indice-1)));                               
+                                int i = vehiculos.indexOf(solicitudes.get(indice-1));
+                                vehiculos.get(i).setDisponibilidad(Estado.Solicitado);                                
+
                                 for(Usuario us : usuarios){
                                     if(us instanceof JefedeTaller){
                                         JefedeTaller jdt = (JefedeTaller) us;
@@ -156,8 +100,8 @@ public class Supervisor extends Usuario{
                             }else if(opc.equals("2")){
                                 System.out.println("\nEstimado supervisor " + getNombre() +", escriba los motivos del rechazo de solicitud de la compra");
                                 System.out.print("\nRespuesta: ");
-                                String respuesta = sc.nextLine();
-                                String mensaje = "Estimado "+clientes.get(indice-1).getNombre()+" la solicitud de compra del vehiculo de la marca "+solicitudes.get(indice-1).getMarca()+" y modelo "+solicitudes.get(indice-1).getModelo()+" ha sido rechazada debido a que: ";
+                                String respuesta = sc.nextLine();                                
+                                String mensaje = "Estimado cliente "+clientes.get(indice-1).getNombre()+" la solicitud de compra del vehiculo de la marca "+solicitudes.get(indice-1).getMarca()+" y modelo "+solicitudes.get(indice-1).getModelo()+" ha sido rechazada debido a que: ";
                                 clientes.get(indice-1).agregarMensaje(mensaje+respuesta);
                                 System.out.println("\nEl mensaje se enviado al comprador"); 
                                 if(vehiculos.contains(solicitudes.get(indice-1))){
@@ -166,6 +110,7 @@ public class Supervisor extends Usuario{
                                 }
                                 solicitudes.remove(indice-1);
                                 clientes.remove(indice-1);
+                                exit = true;
                             }else if(op.equals("3")){
                                 exit = true;
                             }else{
@@ -173,43 +118,18 @@ public class Supervisor extends Usuario{
                             }
                         }
                     }else{
-                    System.out.println("\nElija una opción de la lista de solicitudes\n");
-                    }                
+                        System.out.println("\nElija una opción de la lista de solicitudes\n");
+                    }
                 }else if(op.equals("s")){
-                    break;
+                    salir = true;
                 }else{
                     System.out.println("\nElija un opcion correcta\n");
                 }
-            }else{            
-                break;
             }
-        }  
-    }  
-    public void consultarStock(ArrayList<Vehiculo> vehiculos){                
-        for(Vehiculo vh : vehiculos){            
-            System.out.println(vh.mostrarDatos());
-            System.out.println();
-        }
-    }
-    
-        
-   
-   
-   
-    
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        return hash;
-    }
-
-    
-
-  
-    
-    
-   
-     
+        }else{
+            System.out.println("\nPor ahora no tiene solicitudes de compra\n");
+        }                                
+    }         
 }
        
         
